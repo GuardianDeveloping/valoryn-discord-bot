a8779d00e40b82a20f175ed5896bd52dae01d725
Guardian <giffythaman@gmail.com>
2026-06-21 17:05:22 -1000

    added global commands

--- Valoryn.js
+++ Valoryn.js
@@ -716,7 +716,6 @@ client.once("clientReady", async readyClient => {
     await rest.put(
   Routes.applicationGuildCommands(
     process.env.CLIENT_ID,
-    process.env.GUILD_ID
   ),
   { body: commands }
 );
